cd4ae0c270537aff4d51a0d3099cd7ee15a97699
Will Metzler <metzlerwill@gmail.com>
2024-04-01 14:16:57 -0400

    fix tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { name, city } from "../data/data.js";
 function Home() {
   // update the JSX being returned!
   return <div id="home">Home
-    <h1 style={{ color: 'firebrick' }}>Your {name} is a Web Developer from {city}</h1>
+    <h1 style={{ color: 'firebrick' }}>{name} is a Web Developer from {city}</h1>
   </div>;
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ function NavBar() {
   // update the JSX being returned!
   return <nav>NavBar
     <a href="#home">Home</a>
-    <a href="about">About</a>
+    <a href="#about">About</a>
   </nav>;
 }
 
